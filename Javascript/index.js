7aefca28c9b5bbba3b421dd10d9f00e9c53d76ff
pulchroxloom <dantevbarbieri@gmail.com>
2021-03-13 21:32:36 -0600

    Add comments to Javascript

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,8 @@
+// Prints to the Console
 console.log("Hello World!")
 
+// Edits an element on the Page
 document.getElementById("text").innerText = "Hello World!"
 
+// Does an ALERT popup (these are annoying as hell)
 alert("Hello World!")
